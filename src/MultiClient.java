d0db3018de8a69fe3daf0f5cc6a01be200d2456f
bigboyTaysh <pvtryk.w@gmail.com>
2019-06-04 23:31:13 +0200

    pierwsze poprawki

---
+++ src/MultiClient.java
@@ -0,0 +1,128 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiClient extends Thread {
+
+    List<String> wiadomosci = new ArrayList<String>();
+    List<String> wiadomosci2 = new ArrayList<String>();
+    private Socket socket;
+    private InputStream in;
+    private OutputStream out;
+    private int id;
+    private String login;
+
+
+    public static void main(String[] args) {
+        MultiClient client = new MultiClient();
+
+        client.run2();
+    }
+
+    public void add(String data) {
+        wiadomosci.add(data);
+    }
+
+    public void run2() {
+        try {
+            socket = new Socket("localhost", 2000);
+            System.out.println("Connected To Server ...");
+            //Pobieranie strumieni do gniazda
+            in = socket.getInputStream();
+            out = socket.getOutputStream();
+            //Pobieranie strumienia do standardowego urzadzenia wejscia(klawiatury)
+            //i buforowanie danych z niego przychodzacych
+
+            // login użytkownika
+            BufferedReader fromKeyboard = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("login");
+
+            String data = fromKeyboard.readLine();
+
+            out.write((data).getBytes());
+            out.write("\r\n".getBytes());// dokladanie znak�w konca wiercza
+
+            // potwierdzenie loginu
+            int k = 0;
+            StringBuffer sb = new StringBuffer();
+            //czytanie ze strumienia
+            while ((k = in.read()) != -1 && k != '\n')
+                sb.append((char)k);
+
+
+            login = sb.toString().trim();
+            System.out.println("Twój login: " + login);
+
+            // lista użytkowników
+            System.out.println("Lista użytkowników");
+            k = 0;
+            sb = new StringBuffer();
+            //czytanie ze strumienia
+            while ((k = in.read()) != -1 && k != '\n')
+                sb.append((char)k);
+
+            System.out.println(sb.toString().trim());
+
+            start();
+
+            fromKeyboard = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Format wiadomości -> odbiorca:treść wiadomości");
+
+            while (true) {
+                // sleep(1000);
+                //Czytanie danych ze standardowego urzadzenia wejscia(klawiatury) po linii
+                data = fromKeyboard.readLine();
+
+                //TimeUnit.SECONDS.sleep(1);
+                //KONIEC FAZA 1.
+                out.write((id + ":" + data).getBytes());
+                out.write("\r\n".getBytes());// dokladanie znak�w konca wiercza
+
+            }
+        } catch (UnknownHostException uhe) {
+            System.err.println(uhe);
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
+
+    public void run() {
+        //wysylanie danych strumieniem wyjsciowym do serwera
+
+        try {
+            while (true) {
+
+                int k = 0;
+                StringBuffer sb = new StringBuffer();
+                while (in.available() != 0) {
+                    k = 0;
+                    sb = new StringBuffer();
+                    //czytanie ze strumienia
+                    while ((k = in.read()) != -1 && k != '\n')
+                        sb.append((char) k);
+
+                    String parts1 = sb.toString().trim();
+
+                    wiadomosci2.add(parts1);
+                }
+
+                while (!wiadomosci2.isEmpty()) {
+                    String[] parts = wiadomosci2.get(0).split(":");
+                    System.out.println("Od: " + parts[0]);
+                    System.out.println("Wiadomość: " + parts[1]);
+                    wiadomosci2.remove(0);
+                }
+
+                sleep(500);
+
+            }
+        } catch (UnknownHostException uhe) {
+            System.err.println(uhe);
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/MultiServer.java
@@ -0,0 +1,187 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class MultiServer {
+
+    /**
+     * Lista watkow obslugujacych klientow
+     */
+    List<ClientThread> v = new ArrayList<>();
+
+
+    /**
+     * Glowna metoda programu
+     */
+    public static void main(String[] args) {
+        try {
+            new MultiServer().runServer();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Metoda uruchamiajaca serwer
+     */
+    void runServer() throws IOException {
+        //tworzenie gniazda serwera
+        ServerSocket server = new ServerSocket(2000);
+
+        System.out.println("Server run ... ");
+
+        while (true) {
+
+            //Akceptacja polaczenia;
+            Socket socket = server.accept();
+            System.out.println("New client");
+
+            //Tworzenie watku obsugujacego klienta
+            ClientThread thread = new ClientThread(socket);
+            //dodawanie watku do listy
+            v.add(thread);
+        }
+
+    }
+
+
+    /**
+     * Klasa dziedziczaca po Thread, obslugujaca klientow
+     */
+    class ClientThread extends Thread {
+
+        Socket socket;
+        InputStream in;
+        OutputStream out;
+
+
+        /**
+         * @param socket - zaakceptowanie polaczenie z klientem
+         */
+        ClientThread(Socket socket) throws IOException {
+            this.socket = socket;
+
+            // pobieranie strumieni
+            this.in = socket.getInputStream();
+            this.out = socket.getOutputStream();
+
+            //uruchamianie watku
+            start();
+        }
+
+        /**
+         * Nadpisana metoda w ciele kt�rej znajduja sie instrukcje
+         * wymagajace wykonania w osobnym w�tku
+         */
+        public void run() {
+            try {
+
+                // login użytkownika i potwierdzenie loginu
+                String login = null;
+                int k = 0;
+                StringBuffer sb = new StringBuffer();
+                //czytanie ze strumienia
+                while ((k = in.read()) != -1 && k != '\n')
+                    sb.append((char) k);
+
+
+                System.out.println(v.size());
+                v.get(v.size() - 1).setName(sb.toString().trim());
+                //wysłanie id klienta
+                for (int i = 0; i < v.size(); i++) {
+                    System.out.println(v.get(i).getName());
+                    if (v.get(i).getName().equals(sb.toString().trim())) {
+                        login = v.get(i).getName();
+                    }
+                }
+
+                out.write(login.getBytes());
+                out.write("\n".getBytes());
+
+                // lista użytkowników
+                for (int i = 0; i < v.size(); i++) {
+                    System.out.println(v.get(i).getName());
+                    if (!(v.get(i).getName().equals(sb.toString().trim())) && !(v.get(i).getName().contains("Thread"))) {
+                        out.write((Integer.toString(i + 1) + ". " + v.get(i).getName()).getBytes());
+                    }
+                }
+                out.write("\n".getBytes());
+
+
+                while (isInterrupted() == false) {
+                    k = 0;
+                    sb = new StringBuffer();
+                    //czytanie ze strumienia
+                    while ((k = in.read()) != -1 && k != '\n')
+                        sb.append((char) k);
+
+                    //wysylanie do wszystkich uczestnik�w czatu
+                    System.out.println(sb);
+
+                    String[] parts = sb.toString().split(":");
+                    int id = Integer.parseInt(parts[0]);
+                    int idTo = Integer.parseInt(parts[1]);
+                    String data = parts[2];
+
+                    sendTo(id, idTo, data);
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    in.close();
+                    out.close();
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        /**
+         * Metoda wysylajaca odpowiedz do klienta
+         *
+         * @param data - tablica bajt�w z danymi
+         */
+        void send(byte[] data) {
+            try {
+                //wysylanie danych
+                out.write(data);
+                out.write("\r\n".getBytes());
+
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+
+        /**
+         * Metoda wysylajaca dane do wszystkich klientow w wektorze
+         *
+         * @param data - tablica bajt�w z danymi.
+         */
+        void sendToAll(byte[] data) {
+            v.forEach(t -> t.send(data));
+        }
+
+	/*
+	void sendTo(int id, int idTo, byte[] data){
+		byte[] id2 = String.valueOf(id).getBytes();
+		byte[] destination = new byte[id2.length + data.length];
+		System.arraycopy(id2, 0, destination, 0, id2.length);
+		System.arraycopy(data, 0, destination, id2.length, data.length);
+
+		v.get(idTo).send( destination);
+	}
+	*/
+
+        void sendTo(int id, int idTo, String data) {
+            if (idTo <= v.size()) {
+                v.get(idTo).send((String.valueOf(id) + ":" + data).getBytes());
+                // v.get(idTo).send("\n".getBytes());
+            }
+        }
+    }
+}
